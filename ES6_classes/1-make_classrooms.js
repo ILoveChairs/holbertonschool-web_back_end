a702b97035e1f75da7ccebf16a9d720b0c3fa4be
Matias Davezac <8675@holbertonstudents.com>
2024-07-20 17:31:01 -0300

    Fixed T1, colons

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,11 +1,9 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const myArray = [];
-
-  myArray.push(new ClassRoom(19));
-  myArray.push(new ClassRoom(20));
-  myArray.push(new ClassRoom(34));
-
-  return myArray;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
