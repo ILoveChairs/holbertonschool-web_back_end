1113d77a263a0549413587db8fe0ce76fb800b99
Matias Davezac <8675@holbertonstudents.com>
2024-07-20 17:43:55 -0300

    Fixed T2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -4,4 +4,37 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      throw new TypeError('name must be a string');
+    }
+    this._name = value;
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      throw new TypeError('name must be an number');
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (typeof value === 'number') {
+      throw new TypeError('name must be a number');
+    }
+    this._students = value;
+  }
 }
