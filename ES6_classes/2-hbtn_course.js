ad2d4595a88cb0be88fca82519cdb3d3d8e80620
Matias Davezac <8675@holbertonstudents.com>
2024-07-20 19:19:37 -0300

    Fixed T2?

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,23 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    if (typeof length !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    if (!Array.isArray(students)) {
-      throw new TypeError('Students must be an array');
-    }
-
-    for (const element of students) {
-      if (typeof element !== 'string') {
-        throw new TypeError('Length must be an array of string');
-      }
-    }
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
