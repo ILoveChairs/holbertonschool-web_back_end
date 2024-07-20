fe3d21def58eedcb8f1f6ae6f04503008d4e8a28
Matias Davezac <8675@holbertonstudents.com>
2024-07-20 18:40:22 -0300

    Fixed T2, students is array of strings not ints

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -36,8 +36,8 @@ export default class HolbertonCourse {
       throw new TypeError('Students must be an array');
     }
     for (const element of value) {
-      if (typeof element !== 'number') {
-        throw new TypeError('Length must be an array of numbers');
+      if (typeof element !== 'string') {
+        throw new TypeError('Length must be an array of string');
       }
     }
     this._students = value;
