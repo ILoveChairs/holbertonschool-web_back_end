3ccb3deaa7267e4d430facb852bf9f6b26e72c86
Matias Davezac <8675@holbertonstudents.com>
2024-07-21 19:58:31 -0300

    Added T4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Currency {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
