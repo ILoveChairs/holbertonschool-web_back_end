e2cdc425d9ff97c55de837ceb250c954c783bab8
Matias Davezac <8675@holbertonstudents.com>
2024-07-22 15:01:20 -0300

    Added T4, code sucks though

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-underscore-dangle */
+export default function updateStudentGradeByCity(arrayOfObjects, city, newGrades) {
+  return arrayOfObjects.filter(
+    (student) => student.location === city,
+  ).map(
+    (student) => {
+      const grade = newGrades.filter((grade) => grade.studentId === student.id);
+      const studentCopy = { ...student };
+      if (grade.length === 0) {
+        studentCopy.grade = 'N/A';
+      } else {
+        studentCopy.grade = grade[0].grade;
+      }
+      return studentCopy;
+    },
+  );
+}
