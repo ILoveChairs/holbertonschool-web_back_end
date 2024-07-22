a611dc8de4ef29fc7ccfcc916377d3d89453aa43
Matias Davezac <8675@holbertonstudents.com>
2024-07-22 14:21:35 -0300

    Added T1?

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-underscore-dangle */
+export default function getListStudentIds(arrayOfObjects) {
+  if (!Array.isArray(arrayOfObjects)) {
+    return [];
+  }
+
+  return arrayOfObjects.map((x) => x.id);
+}
