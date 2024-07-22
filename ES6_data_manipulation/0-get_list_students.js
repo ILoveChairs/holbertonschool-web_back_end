9e84c1a5b8902931c5ce76b4a7fdf9048591e8b3
Matias Davezac <8675@holbertonstudents.com>
2024-07-22 14:15:53 -0300

    Fixed T0?

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-underscore-dangle */
+export default function getListStudents() {
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
