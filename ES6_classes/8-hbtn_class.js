d258a75fce7624d62e3a7de7a26dfc2f01950eea
Matias Davezac <8675@holbertonstudents.com>
2024-07-22 11:19:52 -0300

    Added T8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    this._location = value;
+  }
+
+  toString() {
+    return this._location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+}
