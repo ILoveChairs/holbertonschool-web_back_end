1b542a0da9df7c40f1526f9ef33d7c9664f2968d
Matias Davezac <8675@holbertonstudents.com>
2024-07-22 10:05:47 -0300

    Fixed T5, fixed name check

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (!(this.className === 'Building') && this.evacuationWarningMessage === undefined) {
+    if (this.constructor.name !== 'Building' && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
