fbe4546969f3aa3bf223d50b3778227aacdb8df2
Matias Davezac <8675@holbertonstudents.com>
2024-07-22 10:03:17 -0300

    Fixed T5, maybe by class name?

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage === undefined) {
+    if (!(this.className === 'Building') && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
