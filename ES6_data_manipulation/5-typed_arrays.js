9e841cf283e2d45a90833ee86a9cd5ce54b2517c
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 09:40:27 -0300

    Fixed T5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const uint8 = new Uint8Array(buffer);
-  uint8[position] = value;
-  return buffer;
+  const int8 = new Int8Array(length);
+  int8[position] = value;
+  return int8;
 }
