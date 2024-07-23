268f95f9a05a5abb19f3e01b3ecb92c93be99847
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 12:01:42 -0300

    Added T9

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-underscore-dangle */
+export default function groceriesList() {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('ApPastaples', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+}
