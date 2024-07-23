9bf9bd3de1327727fbe29180f8c33b986765b72c
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 09:29:56 -0300

    Fixed T5, int to uint?

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const uint8 = new Int8Array(buffer);
+  const uint8 = new Uint8Array(buffer);
   uint8[position] = value;
   return buffer;
 }
