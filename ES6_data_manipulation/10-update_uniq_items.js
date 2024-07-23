da698d30cc48eb68c835538929b3a8bdebd9faab
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 12:44:51 -0300

    Added T10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-underscore-dangle */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  return map.forEach((value, key, map) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
