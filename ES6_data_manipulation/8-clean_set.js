4157256db7019e2672d9283f6052922e304589f1
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 11:37:06 -0300

    Added T8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-underscore-dangle */
+export default function cleanSet(set, startString) {
+  if (startString.length === 0) {
+    return '';
+  }
+  const output = [];
+  for (const word of set) {
+    const idx = word.indexOf(startString);
+    if (idx === 0) {
+      output.push(word.slice(startString.length));
+    }
+  }
+  return output.join('-');
+}
