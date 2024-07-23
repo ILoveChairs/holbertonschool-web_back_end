eaa256b316b4937cfc4002010b46094d65557d65
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 10:10:43 -0300

    Fixed T7, testing weird stuff

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-underscore-dangle */
 export default function hasValuesFromArray(set, array) {
-  return set.isSupersetOf(new Set(array));
+  return new Set(set).isSupersetOf(new Set(array));
 }
