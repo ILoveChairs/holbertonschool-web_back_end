4c6c32c7bf6e50b3c55a302c24efca406a04d7f1
Matias Davezac <8675@holbertonstudents.com>
2024-07-23 09:50:12 -0300

    Fixed T5, error handling

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const dv = new DataView(new ArrayBuffer(length));
   dv.setInt8(position, value);
   return dv;
