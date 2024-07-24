cabadf6da79379b517c406eddd0bf3d72608235b
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 12:48:19 -0300

    Fixed T6, idk

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.all([
     signUpUser(firstName, lastName).then((response) => response, (err) => err),
     uploadPhoto(fileName).then((response) => response, (err) => err),
-  ]);
+  ]).then((response) => response);
 }
