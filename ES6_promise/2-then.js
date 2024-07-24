811c9e0408fcaada44c2ededba8c93e40b613401
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 09:45:38 -0300

    Fixed T2, success response must be on lowercase

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,4 +1,4 @@
 export default function handleResponseFromAPI(promise) {
   console.log('Got a response from the API');
-  return promise.then(() => ({ body: 'Success', status: 200 }), () => new Error(''));
+  return promise.then(() => ({ body: 'success', status: 200 }), () => new Error(''));
 }
