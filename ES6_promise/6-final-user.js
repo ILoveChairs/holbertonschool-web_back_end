bd5350615243553c97a524c25ae839747f67a22c
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 12:46:17 -0300

    Fixed T6, testing resolve

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,8 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return new Promise((resolve) => {
     resolve([
-      signUpUser(firstName, lastName).then((response) => response, (err) => err),
-      uploadPhoto(fileName).then((response) => response, (err) => err),
+      signUpUser(firstName, lastName).then((response) => response, (err) => err).resolve(),
+      uploadPhoto(fileName).then((response) => response, (err) => err).resolve(),
     ]);
   }).then((response) => response)
     .catch((err) => err);
