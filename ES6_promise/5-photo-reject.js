8692fef4356aeae5ee456d351b9ff4bd9317ef2b
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 10:27:09 -0300

    Added T5

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,3 +1,5 @@
-export default function guardrail() {
-  return '';
+export default function uploadPhoto(fileName) {
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${fileName} cannot be processed`));
+  });
 }
