9be78e1f81d76b93a0797f40e984b904589a73a4
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 10:38:11 -0300

    Added T6?

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,3 +1,8 @@
-export default function guardrail() {
-  return '';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all(signUpUser(firstName, lastName), uploadPhoto(fileName)).then(
+    (response) => response,
+  );
 }
