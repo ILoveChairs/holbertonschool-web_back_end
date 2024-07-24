6b4e6b060633df5759975110da4ec9af80e65eb6
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 11:49:35 -0300

    Fixed T6, testing stuff

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,7 +2,11 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => response)
+  return new Promise((resolve) => {
+    resolve([
+      signUpUser(firstName, lastName).then((response) => response),
+      uploadPhoto(fileName).then((response) => response),
+    ]);
+  }).then((response) => response)
     .catch((err) => err);
 }
