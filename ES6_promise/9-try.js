7067bf404917cc89c5e0ff8f6b3a8e8d6a6d91db
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 13:06:51 -0300

    Added T9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,3 +1,11 @@
-export default function guardrail() {
-  return '';
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
 }
