a4bb77dd01a9e2a05f1dcc5589921caf1d0218c7
Matias Davezac <8675@holbertonstudents.com>
2024-07-24 11:58:04 -0300

    Fixed T6, testing new way

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,11 +2,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+  const resolvedPromised = [];
+  for (const func of promises) {
+    resolvedPromised.push(func.then((response) => response, (err) => err));
+  }
   return new Promise((resolve) => {
-    resolve([
-      signUpUser(firstName, lastName).then((response) => response),
-      uploadPhoto(fileName).then((response) => response),
-    ]);
+    resolve(resolvedPromised);
   }).then((response) => response)
     .catch((err) => err);
 }
