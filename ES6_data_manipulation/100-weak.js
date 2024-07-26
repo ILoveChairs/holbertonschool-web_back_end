bcf363a0d43c7a4509a705129df80f0dfdf7fefe
Matias Davezac <8675@holbertonstudents.com>
2024-07-26 11:44:23 -0300

    Added T100

---
+++ ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let len = weakMap.get(endpoint);
+  if (len === undefined) {
+    len = 0;
+  }
+  if (len >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, len + 1);
+}
