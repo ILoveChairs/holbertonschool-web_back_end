f05a4655811d1c45192459f32d3556f62cfae671
Matias Davezac <8675@holbertonstudents.com>
2024-08-12 12:50:39 -0300

    Initialized new python project (again?) and added T0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 '''
-	Quickdock
+    Quickdock
 '''
 
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    ''' quickdoc '''
+
+    time = random.uniform(0.0, float(max_delay))
+    await asyncio.sleep(time)
+    return time
