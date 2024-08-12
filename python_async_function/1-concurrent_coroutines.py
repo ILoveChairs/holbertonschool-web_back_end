26baec9d96957557bb51df3c22dfab0cd2edb352
Matias Davezac <8675@holbertonstudents.com>
2024-08-12 12:56:52 -0300

    Added T1

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,3 +4,13 @@
 	Quickdock
 '''
 
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    ''' quickdoc '''
+
+    delay_list = []
+    for i in range(n):
+        delay_list.append(wait_random(i))
+    return delay_list
