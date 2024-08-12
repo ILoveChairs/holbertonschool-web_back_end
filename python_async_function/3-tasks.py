1b67b78b5b648c69f6e948fa990ca517aa5b8bb2
Matias Davezac <8675@holbertonstudents.com>
2024-08-12 13:39:51 -0300

    Added T3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 '''
-	Quickdock
+    Quickdock
 '''
 
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    ''' quickdoc '''
+
+    return asyncio.create_task(wait_random(max_delay))
