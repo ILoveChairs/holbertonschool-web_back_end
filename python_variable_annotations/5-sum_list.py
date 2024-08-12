cdecc50161a8822ead858c75965c7908f83fa57c
Matias Davezac <8675@holbertonstudents.com>
2024-08-12 10:38:23 -0300

    Fixed T5, added list annotation from typing module

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,8 +4,10 @@
     Quick doc
 '''
 
+import typing
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: typing.List[float]) -> float:
     ''' quickdoc '''
 
     return sum(input_list)
