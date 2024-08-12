6a73297470028b2f71e29ffdcfce95e7865c5390
Matias Davezac <8675@holbertonstudents.com>
2024-08-12 13:36:38 -0300

    Added T2

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -4,3 +4,17 @@
 	Quickdock
 '''
 
+import time
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    ''' quickdoc '''
+
+    initial_t = time.time()
+    results = asyncio.run(wait_n(n, max_delay))
+    end_t = time.time()
+
+    total_time = end_t - initial_t
+    return total_time / n
