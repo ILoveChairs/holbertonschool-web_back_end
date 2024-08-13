b4b7422494e3eec078750bab0758d11f5dc17272
Matias Davezac <8675@holbertonstudents.com>
2024-08-13 10:20:13 -0300

    Fixed T100, optional return value type annotation

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -9,7 +9,7 @@ from typing import Any
 
 
 # The types of the elements of the input are not known
-def safe_first_element(lst: Any) -> Any:
+def safe_first_element(lst: Any) -> typing.Optional[Any]:
     if lst:
         return lst[0]
     else:
