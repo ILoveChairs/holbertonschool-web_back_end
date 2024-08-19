0916dd07ec791269ee7971c02d7f5cafe6930280
Matias Davezac <8675@holbertonstudents.com>
2024-08-19 12:54:16 -0300

    Fixed T2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,5 +12,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> typing.List[float]:
     ''' quickdoc '''
 
-    print('done')
     return [x async for x in async_generator()]
--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -14,12 +14,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     ''' quickdoc '''
 
-    loop = asyncio.get_event_loop()
     initial_t = time.time()
-    results = asyncio.gather(*[
+    results = await asyncio.gather(*[
         async_comprehension() for _ in range(4)
     ])
-    loop.run_until_complete(results)
     end_t = time.time()
 
     total_time = end_t - initial_t
