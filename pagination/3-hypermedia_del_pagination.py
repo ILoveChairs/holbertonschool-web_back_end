bef9a23b9570050f86ad1e9df3d2e3cc6b0ba65b
Matias Davezac <8675@holbertonstudents.com>
2024-08-19 15:37:31 -0300

    Stashing T3

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,8 +1,66 @@
 #!/usr/bin/env python3
 
-'''
-    Quickdoc
-'''
+"""
+Deletion-resilient hypermedia pagination
+"""
 
-def myFunc():
-    return 1
+import csv
+import math
+from typing import List, Dict, Any
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(
+            self,
+            index: int | None = None,
+            page_size: int = 10
+    ) -> Dict[str, Any]:
+        ''' quickdoc'''
+
+        data = self.dataset()
+        indexed_data = self.indexed_dataset()
+        db_len = len(data)
+
+        if index is None:
+            index = 0
+
+        assert index >= 0 and index <= db_len
+
+        output_data = []
+
+        return {
+            'index': index,
+            'next_index': index + page_size,
+            'page_size': page_size,
+            'data': output_data,
+        }
