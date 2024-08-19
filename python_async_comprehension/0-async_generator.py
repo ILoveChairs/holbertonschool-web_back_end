50342bc21fbbfdfef21167977b5b9ab570d10f53
Matias Davezac <8675@holbertonstudents.com>
2024-08-19 10:36:22 -0300

    Added T0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,6 +4,14 @@
     Quickdoc
 '''
 
+from typing import AsyncGenerator
+from random import randint
+import asyncio
 
-def myFunc() -> None:
+
+async def async_generator() -> AsyncGenerator[int, None]:
     ''' quickdoc '''
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield randint(0, 10)
