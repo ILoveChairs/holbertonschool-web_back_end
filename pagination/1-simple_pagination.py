c9b0f2613ddef48d123bdfd8c3a84547d05dae7b
Matias Davezac <8675@holbertonstudents.com>
2024-08-19 14:16:09 -0300

    Added T1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -4,5 +4,40 @@
     Quickdoc
 '''
 
-def myFunc():
-    return 1
+import typing
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
+    ''' quickdoc '''
+    n = (page - 1) * page_size
+    return n, n + page_size
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+            assert isinstance(page, int) and page > 0
+            assert isinstance(page_size, int) and page_size > 0
+            a, b = index_range(page, page_size)
+            selected_data = self.dataset()[a:b]
+            return selected_data
