aa927fd855b8464e5a03d4df3a3984053502570d
Matias Davezac <8675@holbertonstudents.com>
2024-08-19 14:04:28 -0300

    Added T0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,5 +4,9 @@
     Quickdoc
 '''
 
-def myFunc():
-    return 1
+import typing
+
+
+def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
+    n = (page - 1) * page_size
+    return n, n + page_size
