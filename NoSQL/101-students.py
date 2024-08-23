f41b6103532a8ef95cf9a2d70712cb5f4f5153d7
Matias Davezac <8675@holbertonstudents.com>
2024-08-23 00:25:09 -0300

    Added T101

--- NoSQL/101-students.py
+++ NoSQL/101-students.py
@@ -7,4 +7,16 @@
 def top_students(mongo_collection):
     ''' quickdoc '''
 
-    return [doc for doc in mongo_collection.find()]
+    students = [doc for doc in mongo_collection.find()]
+
+    # AVERAGE CALCULATIONS
+    for student in students:
+        student['averageScore'] = sum(
+            [topic.get('score') for topic in student.get('topics')]
+        )
+
+
+    # SORTING
+    students.sort(key=lambda student: student.get('averageScore', 0))
+
+    return students
