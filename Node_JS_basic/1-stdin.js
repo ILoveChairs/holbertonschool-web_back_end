5d1b90bbac2322ba37bd00b80bcaf2e787a344c1
Matias Davezac <8675@holbertonstudents.com>
2024-09-18 14:57:21 -0300

    Added T1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,5 @@
-const readline = require('node:readline');
+console.log('Welcome to Holberton School, what is your name?');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+process.stdin.on('name', (name) => process.stdout.write(`Your name is: ${name}`));
 
-rl.question('Welcome to Holberton School, what is your name?\n', name => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  rl.close();
-});
+process.stdin.on('pepe', () => console.log('This important software is now closing'));
