1887059d588551b442d5ba171e28ca9ba2d0947c
Matias Davezac <8675@holbertonstudents.com>
2024-09-18 14:59:19 -0300

    Fixed T1?

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,5 +1,5 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('name', (name) => process.stdout.write(`Your name is: ${name}`));
+process.stdin.on('data', (name) => process.stdout.write(`Your name is: ${name}`));
 
-process.stdin.on('pepe', () => console.log('This important software is now closing'));
+process.stdin.on('end', () => console.log('This important software is now closing'));
