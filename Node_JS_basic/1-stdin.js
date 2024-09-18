bdbea9362ea8f0c4feee4dd2a931312bfc83baa6
Matias Davezac <8675@holbertonstudents.com>
2024-09-18 14:41:43 -0300

    Fixed T1, eslint, end of line in in name reoutput

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,9 +4,9 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
 rl.question('Welcome to Holberton School, what is your name?\n', name => {
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\n`);
   rl.close();
   console.log('This important software is now closing');
 });
-
