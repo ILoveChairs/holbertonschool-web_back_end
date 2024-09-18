387c35e6ffba83ee091cbda952754caefe591c2c
Matias Davezac <8675@holbertonstudents.com>
2024-09-18 14:39:22 -0300

    Fixed T1, changed last output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,6 +7,6 @@ const rl = readline.createInterface({
 rl.question('Welcome to Holberton School, what is your name?\n', name => {
   console.log(`Your name is: ${name}`);
   rl.close();
+  console.log('This important software is now closing');
 });
 
-console.log('This important software is now closing');
