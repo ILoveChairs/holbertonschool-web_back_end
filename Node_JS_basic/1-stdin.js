8bab03b1a8cca50ce8f4acf4c38419a439ea028e
Matias Davezac <8675@holbertonstudents.com>
2024-09-18 14:37:32 -0300

    Fixed T1, added new line

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-rl.question('Welcome to Holberton School, what is your name?', name => {
+rl.question('Welcome to Holberton School, what is your name?\n', name => {
   console.log(`Your name is: ${name}`);
   rl.close();
 });
