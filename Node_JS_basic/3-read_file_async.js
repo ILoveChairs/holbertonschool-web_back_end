6ff09eed3b4679b3aa678543cdf51e98fa2618d4
Matias Davezac <8675@holbertonstudents.com>
2024-09-19 14:03:51 -0300

    Added T3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,61 @@
+const fs = require('node:fs/promises');
+
+function countStudents(path) {
+  // Print
+  function printer(data) {
+    const lines = data.split('\n');
+
+    // Remove empty spaces
+    for (const lineN in lines) {
+      if (Object.hasOwn(lines, lineN)) {
+        if (lines[lineN].trim() === '') {
+          lines.splice(lineN, 1);
+        }
+      }
+    }
+
+    // Number of students
+    const studentsTotal = lines.length - 1;
+    console.log(`Number of students: ${studentsTotal}`);
+
+    // Divide into students
+    lines.splice(0, 1);
+    const students = [];
+    for (const line of lines) {
+      students.push(line.split(','));
+    }
+
+    // Divide into fields
+    const fields = {};
+    for (const student of students) {
+      // [0] is first name, [3] is field
+      if (!(student[3] in fields)) {
+        fields[student[3]] = [];
+      }
+      fields[student[3]].push(student[0]);
+    }
+
+    // Print classes
+    for (const field in fields) {
+      if (Object.hasOwn(fields, field)) {
+        const numOfStudents = fields[field].length;
+        const listOfFirstNames = fields[field].join(', ');
+        console.log(`Number of students in ${field}: ${numOfStudents}. List: ${listOfFirstNames}`);
+      }
+    }
+  }
+
+  // Read csv
+
+  return async () => {
+    try {
+      const data = await fs.readFile(path, { encoding: 'utf8' });
+      console.log(data);
+      printer(data);
+    } catch (err) {
+      throw Error('Cannot load the database');
+    }
+  }
+}
+
+module.exports = countStudents;
